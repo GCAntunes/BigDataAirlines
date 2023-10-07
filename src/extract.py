f714a8d5a14726fc82e7fb682a4846977473b3e4
GCA <antunes@pop-os.localdomain>
2023-10-07 15:52:11 -0300

    Adiciona função extract_air_csv ao extract.py

--- src/extract.py
+++ src/extract.py
@@ -6,8 +6,16 @@ def extract_vra_json(path: str)  -> pd.DataFrame:
     all_files =  glob.glob(path)
     df_vra = pd.concat((pd.read_json(file, encoding = 'utf_8_sig',dtype=True) for file in all_files) , ignore_index=True) 
     return df_vra 
-    
+
+def extract_air_csv(path: str)  -> pd.DataFrame:
+
+    all_files =  glob.glob(path)
+    df_air_cia = pd.concat((pd.read_csv(file, sep = ';') for file in all_files) , ignore_index=True) 
+    return df_air_cia
+
 if __name__ == '__main__':
-    df_vra = extract_vra_json('VRA*/*.json')
+    df_vra = extract_vra_json('./data/VRA/*.json')
     print(df_vra)
+    df_air_cia = extract_air_csv('./data/AIR_CIA/*.csv')
+    print(df_air_cia)
     
