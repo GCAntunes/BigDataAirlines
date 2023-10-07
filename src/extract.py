7e396745171e654043f4fd1bfa5d81415be311cb
GCA <antunes@pop-os.localdomain>
2023-10-07 16:02:15 -0300

    Adiciona docstrings

--- src/extract.py
+++ src/extract.py
@@ -1,13 +1,33 @@
 import glob
 import pandas as pd
 
+"""
+Módulo com as funções de extração.
+"""
+
 def extract_vra_json(path: str)  -> pd.DataFrame:
+    """
+    Função que lê a pasta de json's de VRA e retorna um único Pandas Dataframe
+    
+    Params:
+        path (str): Caminho dos jsons de VRA
+    Return:
+        df_vra (pd.DataFrame): Dataframe resultante da compilação de todos os jsons
+    """
 
     all_files =  glob.glob(path)
     df_vra = pd.concat((pd.read_json(file, encoding = 'utf_8_sig',dtype=True) for file in all_files) , ignore_index=True) 
     return df_vra 
 
 def extract_air_csv(path: str)  -> pd.DataFrame:
+    """
+    Função que lê a pasta de csv's de AIR_CIA e retorna um único Pandas Dataframe
+    
+    Params:
+        path (str): Caminho dos csv's de AIR_CIA
+    Return:
+        df_vra (pd.DataFrame): Dataframe resultante da compilação de todos os csv's
+    """
 
     all_files =  glob.glob(path)
     df_air_cia = pd.concat((pd.read_csv(file, sep = ';') for file in all_files) , ignore_index=True) 
