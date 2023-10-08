378a842983fe880ae2c503ec4b69c2ac7d489a24
GCA <antunes@pop-os.localdomain>
2023-10-08 17:58:13 -0300

    Adiciona teste para checar existência dos arquivos de origem

--- tests/transform_test.py
+++ tests/transform_test.py
@@ -1,9 +1,32 @@
 import pytest
 import requests
 import os
+import glob
 
 from src.transform import snake_case
 
+param_list = [
+    ('data/AIR_CIA/ANAC_*.csv'),
+    ('data/VRA/VRA_*.json'),
+]
+@pytest.mark.parametrize('input', param_list)
+def test_if_input_files_exist(input):
+    assert glob.glob(input)
+
+def test_api_key_env():
+    api_key = os.getenv('API_KEY')
+    assert api_key != None
+    assert type(api_key) == str
+  
+def test_integration_aiport_api():
+    url = 'https://airport-info.p.rapidapi.com/airport'
+    params = params = {'icao': '3166'}
+    headers = {
+        'X-RapidAPI-Key': os.getenv('API_KEY'),
+        'X-RapidAPI-Host': 'airport-info.p.rapidapi.com',
+    }
+    response = requests.get(url, params=params, headers=headers)
+    assert response.status_code == 200
 
 @pytest.mark.parametrize(
     'input, expected',
@@ -18,12 +41,4 @@ from src.transform import snake_case
 def test_snake_case(input, expected):
     assert snake_case(input) == expected
 
-def test_integration_aiport_api():
-    url = 'https://airport-info.p.rapidapi.com/airport'
-    params = params = {'icao': '3166'}
-    headers = {
-        'X-RapidAPI-Key': os.getenv('API_KEY'),
-        'X-RapidAPI-Host': 'airport-info.p.rapidapi.com',
-    }
-    response = requests.get(url, params=params, headers=headers)
-    assert response.status_code == 200
+
