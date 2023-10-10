f8c183c7b325c2ac0d04e4841588ccbe5b688cab
GCA <antunes@pop-os.localdomain>
2023-10-10 13:26:35 -0300

    Refatora processo para salvar em parquet na camada analítica

---
+++ src/analytic/load.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+
+def df_to_parquet(df: pd.DataFrame, dir_path: str, filename: str):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    df.to_parquet(
+        f'{dir_path}/{filename}.snappy.parquet', compression='snappy'
+    )
--- src/analytic/pipeline_analytic.py
+++ src/analytic/pipeline_analytic.py
@@ -1,5 +1,6 @@
 from extract import get_df_from_parquet
 from transform import view_cia
+from load import df_to_parquet
 
 if __name__ == '__main__':
     df_air_cia = get_df_from_parquet('./output/AIR_CIA/')
@@ -8,8 +9,4 @@ if __name__ == '__main__':
     
     df_view_cia = view_cia(df_air_cia, df_vra, df_aerodromos)
     
-    print(df_view_cia)
-    
-    print(df_view_cia.columns)
-    
-    df_view_cia.to_parquet('output/VIEW_CIA/view_cia.snappy.parquet', compression='snappy')
+    df_to_parquet(df_view_cia, './output/VIEW_CIA', 'view_cia')
