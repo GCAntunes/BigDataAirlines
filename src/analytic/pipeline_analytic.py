7580e587cccf29d38346da88afa956ef99232e85
GCAntunes <gantunes.engcivil@gmail.com>
2023-10-11 12:27:01 -0300

    Adiciona função view_aero

--- src/analytic/pipeline_analytic.py
+++ src/analytic/pipeline_analytic.py
@@ -1,6 +1,6 @@
 from extract import get_df_from_parquet
 from load import df_to_parquet
-from transform import view_cia
+from transform import view_cia, view_aero
 
 from rich.progress import Progress
 
@@ -16,7 +16,11 @@ if __name__ == '__main__':
     progress.update(pipeline_task, advance=25, description= "Gerando df_view_cia")
     df_view_cia = view_cia(df_air_cia, df_vra, df_aerodromos)
     
-    progress.update(pipeline_task, advance=65, description= "Salvando os .parquet's na camada Analítica")
+    progress.update(pipeline_task, advance = 35)
+    df_ranking_aero_cia = view_aero(df_air_cia, df_vra, df_aerodromos)    
+    
+    progress.update(pipeline_task, advance=30, description= "Salvando os .parquet's na camada Analítica")
     df_to_parquet(df_view_cia, './output/VIEW_CIA', 'view_cia')
+    df_to_parquet(df_ranking_aero_cia, './output/RANKING_AERO_CIA', 'ranking_aero_cia')
     progress.update(pipeline_task, advance=10, description= 'Finalizado!')
     progress.stop()
--- src/analytic/transform.py
+++ src/analytic/transform.py
@@ -19,13 +19,56 @@ def view_cia(df_air_cia: pd.DataFrame, df_vra: pd.DataFrame, df_aerodromos:pd.Da
                                 .agg(['count'])
                                # .sort_values(by=['razao_social', 'count'], axis = 0, ascending=[True, False])                                                        
                                 )
-    
-    
+        
     df_ranking_cia = (df_ranking_cia.assign(rank =df_ranking_cia.groupby('razao_social')['count'].rank(method='first', ascending=False),
                                             rota = df_ranking_cia['aeroporto_origem'].str.cat(df_ranking_cia['aeroporto_destino'], '-'))
                                      .query("rank == 1")
                                      .reset_index(drop = True)
+                                     .drop(columns='rank')
+                                     .rename(columns={'count':'num_viagens'})
                                             )
-    
-    
+        
     return df_ranking_cia
+
+def view_aero(df_air_cia: pd.DataFrame, df_vra: pd.DataFrame, df_aerodromos:pd.DataFrame) -> pd.DataFrame:
+       df_air_cia = df_air_cia[['icao', 'razao_social']]
+       df_vra = df_vra[['icao_aerodromo_origem', 'icao_aerodromo_destino', 'situacao_voo', 'icao_empresa_aerea']]
+       df_lat_lon = df_aerodromos[['icao', 'latitude', 'longitude']]
+       df_aerodromos = df_aerodromos[['icao', 'name']]       
+       
+       df_decolagens = (df_air_cia.merge(df_vra, 'left', left_on= 'icao', right_on='icao_empresa_aerea', suffixes=('', '_vra'))
+                                  .merge(df_aerodromos, 'right', left_on='icao_aerodromo_origem', right_on='icao', suffixes=('', '_aero'))
+                                  .query("situacao_voo == 'REALIZADO'")
+                                  .drop(columns = ['icao_aerodromo_destino', 'icao_aero', 'situacao_voo'])
+                                  .groupby(['name', 'icao', 'razao_social', 'icao_aerodromo_origem', 'icao_empresa_aerea'], as_index = False)['razao_social']
+                                  .agg(['count'])
+                                  .rename(columns={'count':'num_decolagens'})
+                                   )
+       
+       df_pousos = (df_air_cia.merge(df_vra, 'left', left_on= 'icao', right_on='icao_empresa_aerea', suffixes=('', '_vra'))
+                              .merge(df_aerodromos, 'right', left_on='icao_aerodromo_destino', right_on='icao', suffixes=('', '_aero'))
+                              .query("situacao_voo == 'REALIZADO'")
+                              .drop(columns = ['icao_aerodromo_origem', 'icao_aero', 'situacao_voo'])
+                              .groupby(['name', 'icao', 'razao_social', 'icao_aerodromo_destino', 'icao_empresa_aerea'], as_index = False)['razao_social']
+                              .agg(['count'])
+                              .rename(columns={'count':'num_pousos'})
+                               )
+       
+       df_ranking_aero_cia = (df_decolagens.merge(df_pousos, 'inner', left_on=['icao_aerodromo_origem', 'icao_empresa_aerea'], right_on=['icao_aerodromo_destino', 'icao_empresa_aerea'], suffixes=('', '_pou'))
+                                           #.assign(total_operacoes = df_decolagens.num_decolagens + df_decolagens.num_pousos)
+                                           .drop(columns = ['name_pou', 'icao_pou', 'razao_social_pou', 'icao_empresa_aerea', 'icao_aerodromo_destino', 'icao'])
+                                           .rename(columns = {'icao_aerodromo_origem': 'icao_aerodromo'}))
+       
+       df_ranking_aero_cia = df_ranking_aero_cia.assign(total_operacoes = df_ranking_aero_cia.num_decolagens +df_ranking_aero_cia.num_pousos)
+       df_ranking_aero_cia = (df_ranking_aero_cia.assign(rank = df_ranking_aero_cia.groupby('name')['total_operacoes'].rank('first', ascending = False))
+                                                 .query("rank == 1")
+                                                 .merge(df_lat_lon, 'inner', left_on='icao_aerodromo', right_on='icao')
+                                                 .drop(columns = 'rank')
+                                                 )
+                                                                                                         
+       
+       
+       
+       return df_ranking_aero_cia
+       
+       
